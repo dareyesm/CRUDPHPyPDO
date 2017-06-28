c87791ce8b6543947f7e97bf0f6d6b71d94dd75f
dareyesm <dareyesm@hotmail.com>
2017-06-28 12:54:41 -0500

    Como hacer un CRUD con los PDO de PHP y MySQL

---
+++ nuevo_exe.php
@@ -0,0 +1,22 @@
+<?php
+
+//nos conectamos a la BD
+
+require'class/database.php';
+$objData = new Database();
+
+//print_r($_POST);
+
+$sth = $objData->prepare('INSERT INTO datos values (:idDato, :nombre, :descri) ');
+
+$idDato = '';
+$nombre = $_POST['nombre'];
+$descri = $_POST['descri'];
+
+$sth->bindParam(':idDato', $idDato);
+$sth->bindParam(':nombre', $nombre);
+$sth->bindParam(':descri', $descri);
+
+$sth->execute();
+
+header('location: index.php');
